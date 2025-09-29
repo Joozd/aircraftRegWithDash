3ebf73761a6bd25ea36a41354c80ee32683780b2
joozd <euroshopper@gmail.com>
2025-09-29 19:05:42 +0200

    Bugfix - no more USA crashing on registrations without digits

--- src/main/kotlin/countryrules/Unitedstates.kt
+++ src/main/kotlin/countryrules/Unitedstates.kt
@@ -1,6 +1,6 @@
 package nl.joozd.aircraftreg.countryrules
 
-object Unitedstates: CountryRulesImpl('N') {
+internal object Unitedstates: CountryRulesImpl('N') {
     /**
      * true if [stringWithoutDash] falls under these rules.
      * This includes partial strings, e.g. "ZAA" should be in "ZAAAA"-"ZAZZZ"
@@ -13,6 +13,7 @@ object Unitedstates: CountryRulesImpl('N') {
      */
     override fun getCorrectString(stringWithoutDash: String): String{
         val digits = stringWithoutDash.filter { it.isDigit() }
+        if(digits.isEmpty()) return stringWithoutDash
         if(digits !in stringWithoutDash) return stringWithoutDash // all digits must be in one group, this is an invalid registration
         val shortenedDigits = digits.toInt().toString()
         return stringWithoutDash.replace(digits, shortenedDigits)
